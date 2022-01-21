8e48fd2053af3d3ba97e737c650aa620026c34b2
Daniel <daniel.mikami@gmail.com>
2022-01-21 15:49:03 -0300

    print

--- print.php
+++ print.php
@@ -16,7 +16,216 @@
 
   require_once 'basic_dan_machine.php';
 
+  $view = true;
+  if($view){
+    $_PRINT = [
+      'esc' => "<br><br>",
+      'codeutf8' => "",
+      'cortaTotal'		=> "\n<div style='color:red;'>".mb_str_pad("",48,"-",STR_PAD_BOTH).'</div><br><br>',
+      'cortaParcial'		=> "\n<div style='color:red;'>".mb_str_pad("||",48,"-",STR_PAD_BOTH).'</div><br><br>',
+      'avancaPapel'		=> "",
+      'diminuiLineSpace'	=> "",
+
+      'n_on'		=> "<b>",
+      'n_off'		=> "</b>",
+
+      's_on'		=> "<u>",
+      's_off'		=> "</u>",
+
+      'i_on' => "<i>",
+      'i_off' => "</i>",
+
+      'emphase' => "",
+
+      'center' => "<div style='align: center;'>",
+      'center_off' => '</div>', //Apenas para Debug
+      'left'   => "",
+      'left_off' => '', //Apenas para Debug
+      'right'   => "",
+      'right_off' => '', //Apenas para Debug
+
+      'tamanhoDuploOn'		=> "<div style='transform: scale(1,1.5);'>",
+      'tamanhoDuploOff'	=> "</div>",
+
+      'melhoraQualidade'	=> "",
+      'diminuiQualidade'	=> "",
+      'print_works' => '',
+    ];
+  } else {
+    $_PRINT = [
+      'esc'	 		=> "\x0", // toda impressão deve começar com esse caracter de escape;
+      'codeutf8' => "\x1B\x74\x08",
+      'cortaTotal'		=> "\x1B\x69",
+      'cortaParcial'		=> "\x1B\x6D",
+      'avancaPapel'		=> "\x0C",
+      'diminuiLineSpace'	=> "\x1B\x33\x12",
+
+      'n_on'		=> "\x1B\x45",
+      'n_off'		=> "\x1B\x46",
+
+      's_on'		=> "\x1B\x2D\x01",
+      's_off'		=> "\x1B\x2D\x00",
+
+      'i_on' => "\x1B\x34",
+      'i_off' => "\x1B\x35",
+
+      'emphase' => "\x1B\x56",
+
+      'center' => "\x1B\x61\x01",
+      'center_off' => '', //Apenas para Debug
+      'left'   => "\x1B\x61\x00",
+      'left_off' => '', //Apenas para Debug
+      'right'   => "\x1B\x61\x02",
+      'right_off' => '', //Apenas para Debug
+
+      'tamanhoDuploOn'		=> "\x1b\x64\x01",
+      'tamanhoDuploOff'	=> "\x1b\x64\x00",
+
+      'melhoraQualidade'	=> "\x1D\xF9\x2D\x01",
+      'diminuiQualidade'	=> "\x1D\xF9\x2D\x00",
+      'print_works' => '',
+    ];
+  }
+
+  $impressora_endereco_na_rede = $_REQUEST['ip'];
+  $impressora_porta_padrao = $_REQUEST['port']; // Porta em que a impressora fica escutando por padrão
+  // http://localhost/tcr8_machine/print.php?action=mp4200&ip=192.168.0.99&port=9100&text=oi
+  // echo "$impressora_endereco_na_rede <br> $impressora_porta_padrao <br>";
+
+  function exec_print($header,$sys_header,$body,$sys_body,$footer='',$sys_footer = ''){
+  	extract($GLOBALS);
+  	$data = array();
+  	try{
+      $title    = cfg['store']['label'];
+      $loja			= cfg['store']['razao'];
+      $local    = cfg['store']['local'];
+      $sigla    = cfg['store']['abr'];
+      $store_abr = cfg['store']['abr'];
+
+      $cnpj     = mask(cfg['store']['cnpj'],'##.###.###/####-##');
+      $address  = cfg['store']['logradouro'].', '.cfg['store']['numero'].' - '.cfg['store']['compl'];
+      $address2 = 'Bairro: '.cfg['store']['bairro'].' - '.cfg['store']['municipio'].' / '.cfg['store']['uf'];
+      $impressora_porta_padrao = (int)cfg['printer']['cupom']['port']; // Porta em que a impressora fica escutando por padrão
+
+  		$init  = $_PRINT['esc'] . $_PRINT['codeutf8'] . $_PRINT['melhoraQualidade'] . $_PRINT['diminuiLineSpace'];
+  		$cust_header  = $_PRINT['n_on'] . mb_str_pad($title,48," ",STR_PAD_BOTH) . $_PRINT['n_off'] . "\n";
+  		$cust_header .= $_PRINT['n_on'] . mb_str_pad("www.infinifashion.com.br",48," ",STR_PAD_BOTH) . $_PRINT['n_off'] ."\n";
+  		$cust_header .= $message . "\n";
+
+  		$cust_header .= $_PRINT['left'] . $address . "\n";
+  		$cust_header .= $_PRINT['left'] . $address2 . "\n";
+  		$cust_header .= $_PRINT['s_on'] . mb_str_pad($loja, 30) . mb_str_pad($cnpj, 18). $_PRINT['s_off'] . "\n";
+  		$cust_header .= $header;
+  		$cust_header .= $_PRINT['left'] . "\n";
+
+  		$cust_footer  = "\n";
+  		$cust_footer .= $footer;
+
+  		$impressao = $init . $cust_header . $body . $footer;
+  		if($sys_body != '') $impressao .= $_PRINT['cortaParcial'] . $sys_header . $sys_body . $sys_footer;
+  		$impressao .= $_PRINT['cortaTotal'];
+
+  		if(!$view){
+  			$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+  			if ($socket === false) throw new Exception( "socket_create() falha: motivo: " . socket_strerror(socket_last_error()) );
+  			$result = socket_connect($socket, $impressora_endereco_na_rede, $impressora_porta_padrao);
+  			if ($result === false) throw new Exception( "socket_connect() falha: motivo: ($result) " . socket_strerror(socket_last_error($socket)) );
+
+  			socket_write($socket, $impressao, strlen($impressao));
+  			socket_close($socket);
+  		} else {
+  			$html_debug = "<div class='teste' style='width: 100%;margin: 0 auto;background:white'><pre>".$impressao."</pre></div>";
+  			$data['html'] = '<div style="margin: 0 auto;">' . $html_debug . '</div>';
+  		}
+
+  		$data['Result'] = 'OK';
+      $data['success'] = true;
+      $data['message'] = 'Impressão Concluída';
+  		$data['type'] = 'success';
+    }catch (Exception $e){
+      $data['success'] = false;
+      $data['message'] = $e->getMessage();
+    }
+  	$data['printer'] = $impressora_endereco_na_rede;
+  	$data['port']    = $impressora_porta_padrao;
+    $data['msg'] = $data['message'];
+    return $data;
+    exit;
+  }
+
+  function exec_sprint($header='',$body='',$footer='',$corte_total = true){ //simple print
+  	extract($GLOBALS);
+  	$data = array();
+  	try{
+  		$corte = $corte_total ? $_PRINT['cortaTotal'] : $_PRINT['cortaParcial'];
+  		$init      = $_PRINT['esc'] . $_PRINT['codeutf8'] . $_PRINT['melhoraQualidade'] . $_PRINT['diminuiLineSpace']  ;
+  		$impressao = $init . $header . $body . $footer . $corte;
+
+  		if(!$view){
+  			$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+  			if ($socket === false) throw new Exception( "socket_create() falha: motivo: " . socket_strerror(socket_last_error()) );
+  			$result = socket_connect($socket, $impressora_endereco_na_rede, $impressora_porta_padrao);
+  			if ($result === false) throw new Exception( "socket_connect() falha: motivo: ($result) " . socket_strerror(socket_last_error($socket)) );
+
+  			socket_write($socket, $impressao, strlen($impressao));
+  			socket_close($socket);
+  		} else {
+  			$html_debug = "<div class='teste' style='width: 100%;margin: 0 auto; background:white'><pre>".$impressao."</pre></div>";
+  			$data['html'] = '<pre><div style="margin: 0 auto;">' . $html_debug . '</div></pre>';
+  		}
+
+  		$data['Result'] = 'OK';
+      $data['success'] = true;
+      $data['message'] = 'Impressão Concluída';
+  		$data['type'] = 'success';
+    }catch (Exception $e){
+      $data['success'] = false;
+      $data['message'] = $e->getMessage();
+    }
+    $data['msg'] = $data['message'];
+    return $data;
+    // echo $data['html'];
+    exit;
+  }
+
   tcr8_function();
+
+  function mp4200(){
+    $data = array();
+    extract($GLOBALS);
+    try{
+      // if( empty($_FILES['data']) ) throw new Exception( 'Não foi encontrado nenhum arquivo texto com dados de variável' );
+      // $_DADOS = json_decode(file_get_contents($_FILES['data']['tmp_name']),true);
+
+      $header = $body = $footer = "";
+
+  		$text = $_REQUEST['text'];
+  		$text = preg_split('/<br[^>]*>/i', $text);
+  		$body.= $_PRINT['tamanhoDuploOn'].$_PRINT['n_on'];
+  		foreach ($text as $linha) {
+  			$body .= wordwrap($linha, 48, "\n");
+  			$body .= "\n";
+  		}
+  		$body.= $_PRINT['tamanhoDuploOff'].$_PRINT['n_off'];
+
+  		$body .= "\n";
+  		$body .= "\n";
+
+      $data['print']   = exec_sprint($header,$body,$footer);
+
+
+      $data['success'] = true;
+      $data['close_all'] = false;
+      $data['reload']    = false;
+      $data['message'] = 'NFE via PHP Emitido';
+    } catch (Exception $e){
+      $data['success'] = false;
+      $data['message'] = $e->getMessage();
+    }
+    $data['msg'] = $data['message'];
+    echo json_encode($data);
+    exit;
+  }
 
   function generate_sat(){
     $data = array();
